0830d44c0e0cee39cc207d71a7da1eba7a36122b
psingh <psingh@parkar.consulting>
2017-01-19 11:50:52 +0530

    updated theme for comment to category

--- sites/all/themes/splash/templates/node/node--article--teaser.tpl.php
+++ sites/all/themes/splash/templates/node/node--article--teaser.tpl.php
@@ -91,7 +91,10 @@
         <ul>
             <li><span class="date"><?php print date('d F Y', $created);?></span></li>
             <li><span>Post by <?php print strip_tags($name);?></span></li>
-            <li><span class="comments"><?php print $comment_count;?> Comments</span></li>
+            <?php if (isset($blog_categories)):?>
+				<li><span class="category"> <?php print $blog_categories;?></span> </li>
+			<?php endif;?>
+		<!-- <li><span class="comments"><?php //print $comment_count;?> Comments</span></li> -->
         </ul>
     </div>
     <h6 class="post-title">
